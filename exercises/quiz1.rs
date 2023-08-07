88c1cec28dff6f0712c1708692678b58b901eea3
liujianjin <liujianjin2001@163.com>
2023-08-07 22:53:31 +0800

    modified:   exercises/functions/functions1.rs 	modified:   exercises/functions/functions2.rs 	modified:   exercises/functions/functions3.rs 	modified:   exercises/functions/functions4.rs 	modified:   exercises/functions/functions5.rs 	modified:   exercises/if/if1.rs 	modified:   exercises/if/if2.rs 	modified:   exercises/if/if3.rs 	modified:   exercises/intro/intro2.rs 	modified:   exercises/primitive_types/primitive_types1.rs 	modified:   exercises/primitive_types/primitive_types2.rs 	modified:   exercises/primitive_types/primitive_types3.rs 	modified:   exercises/primitive_types/primitive_types4.rs 	modified:   exercises/primitive_types/primitive_types5.rs 	modified:   exercises/primitive_types/primitive_types6.rs 	modified:   exercises/quiz1.rs 	modified:   exercises/variables/variables1.rs 	modified:   exercises/variables/variables2.rs 	modified:   exercises/variables/variables3.rs 	modified:   exercises/variables/variables4.rs 	modified:   exercises/variables/variables5.rs 	modified:   exercises/variables/variables6.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(c:i32) -> i32 {
+    if c > 40{
+        c
+    }else{
+        c * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
