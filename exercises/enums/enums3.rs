d9fe8fef03852ec334b0e52c9c64466651f3ff11
liujianjin <liujianjin2001@163.com>
2023-08-12 21:55:54 +0800

    modified:   exercises/enums/enums1.rs 	modified:   exercises/enums/enums2.rs 	modified:   exercises/enums/enums3.rs 	modified:   exercises/structs/structs1.rs 	modified:   exercises/structs/structs2.rs 	modified:   exercises/structs/structs3.rs

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move (Point),
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 struct Point {
