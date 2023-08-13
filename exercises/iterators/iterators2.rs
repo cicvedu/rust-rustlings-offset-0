c728b8b486d2eda44771fccbbb9b153ec2bf063f
liujianjin <liujianjin2001@163.com>
2023-08-13 19:28:16 +0800

    modified:   exercises/iterators/iterators2.rs 	modified:   exercises/iterators/iterators3.rs

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +15,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().chain(c).collect(),
     }
 }
 
@@ -24,15 +24,20 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
 // Apply the `capitalize_first` function again to a slice of string slices.
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
+
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut result = capitalize_words_vector(words).join(" ");
+    while result.contains("  ") {
+        result = result.replace("  ", " ");
+    }
+    result
 }
 
 #[cfg(test)]
